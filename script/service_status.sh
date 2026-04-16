8eb0d52ea88c5af4dea2910fe90052b8cbcfde1a
sy0218 <sy02229@naver.com>
2026-04-16 09:04:33 +0900

    20260416

--- script/service_status.sh
+++ script/service_status.sh
@@ -3,7 +3,7 @@
 # 출력 디렉토리
 OUTDIR="/application/node_exporter/textfile_collector"
 # 체크할 서비스 목록
-SERVICES=("filebeat" "docker" "kafka-server" "zookeeper-server")
+SERVICES=("docker" "kafka-server" "zookeeper-server")
 
 # 각 서비스 상태 체크
 for service in "${SERVICES[@]}";
